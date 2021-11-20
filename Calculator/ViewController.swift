6ddd582f450067dc35212dd27684e440833052c1
AndresGutierrez <c0dingw.dre@gmail.com>
2021-11-20 15:02:33 -0800

    Create functionality for CalcButtons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var didFinishTyping = true
-    
+  
     @IBOutlet weak var displayLabel: UILabel!
     
+    var didFinishTyping = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
     
+        didFinishTyping = true
+        
+        guard let dubNumb = Double(displayLabel.text!) else {
+            fatalError("Error converting text to Double!")
+        }
+        if let Button = sender.currentTitle{
+            switch Button{
+            case "AC":
+                displayLabel.text = "0"
+            case "+/-":
+                displayLabel.text = String(dubNumb * -1)
+            case "%":
+                displayLabel.text = String(dubNumb * 0.01)
+            default:
+                fatalError("Unknown Button Pressed!")
+            }
+        }
+        
     }
 
     
